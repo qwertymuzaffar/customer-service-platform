1233ad855e50c6ae153fd0cbbcea0a8da53d8968
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-11-23 14:36:47 -0500

    Initial commit

---
+++ src/main/java/com/retailstore/customer_service_platform/CustomerServicePlatformApplication.java
@@ -0,0 +1,14 @@
+package com.retailstore.customer_service_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServicePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServicePlatformApplication.class, args);
+		System.out.println("Hello, world!");
+	}
+
+}
---
+++ src/test/java/com/retailstore/customer_service_platform/CustomerServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailstore.customer_service_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServicePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
