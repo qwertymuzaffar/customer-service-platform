cec4f64e00d8b5157a691f71988722ce9f429466
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-11-23 15:14:30 -0500

    Remove Spring Boot

--- src/test/java/com/retailstore/customer_service_platform/CustomerServicePlatformApplicationTests.java
+++ src/test/java/com/retailstore/customer_service_platform/CustomerServicePlatformApplicationTests.java
@@ -1,9 +1,8 @@
 package com.retailstore.customer_service_platform;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CustomerServicePlatformApplicationTests {
 
 	@Test
